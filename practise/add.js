8b5c27b5bb3b5df304a56163f710d4c3a5dd5fb5
swathiperla <98946392+perlaritgithub@users.noreply.github.com>
2022-04-04 18:10:34 +0530

    Add files via upload

---
+++ practise/add.js
@@ -0,0 +1,18 @@
+
+/*function sum(a,b){
+    return a+b;
+}
+var c=sum(10,20);
+console.log(c);
+
+function sum(a,b){
+    return a-b;
+}
+var c=sum(10,20);
+console.log(c);
+
+function sum(a,b){
+    return a*b;
+}
+var c=sum(10,20);
+console.log(c);*/
